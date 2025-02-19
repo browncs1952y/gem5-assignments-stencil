36ae2b042ff685cceb8ba1a3d3904b40ba4c5a31
mziz <milda_zizyte@brown.edu>
2025-02-19 18:26:08 -0500

    update short prog

--- tests/test-progs/cache-tests/prog_short.c
+++ tests/test-progs/cache-tests/prog_short.c
@@ -1,11 +1,9 @@
-#include "util.h"
-
 int
 main(int argc, char **argv)
 {
-    char arr[1 << 20];
+    char arr[256];
 
-    for (int i = (rand() % (1 << 10)); i < 256; i++) {
-        arr[i]++;
+    for (int i = 0; i < 256; i++) {
+        arr[i] = i;
     }
 }

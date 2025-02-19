f4389c256675165660aaaa786df0743e79f8fc95
mziz <milda_zizyte@brown.edu>
2025-02-19 18:28:11 -0500

    randomize short prog

--- tests/test-progs/cache-tests/prog_short.c
+++ tests/test-progs/cache-tests/prog_short.c
@@ -1,9 +1,11 @@
+#include "util.h"
+
 int
 main(int argc, char **argv)
 {
     char arr[256];
 
     for (int i = 0; i < 256; i++) {
-        arr[i] = i;
+        arr[i] = (rand() % (1 << 16));
     }
 }
